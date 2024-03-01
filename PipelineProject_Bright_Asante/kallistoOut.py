717522c15a3ddb9289e42fdd568366cc87c69579
abright087 <abright087@gmail.com>
2024-03-01 12:36:27 -0600

    My results Folder

---
+++ PipelineProject_Bright_Asante/kallistoOut.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+sample condition path
+SRX2896360(Donor1) 2dpi results/SRX2896360
+SRX2896363(Donor1) 6dpi results/SRX2896363
+SRX2896374(Donor3) 2dpi results/SRX2896374
+SRX2896375(Donor3) 6dpi results/SRX2896375
